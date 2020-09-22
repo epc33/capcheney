f5270dcdccfaee9f98cc28a084f13ce720eba863
Evan Cheney <70952698+epc33@users.noreply.github.com>
2020-09-22 08:35:45 -0400

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
 productid: {type: String, unique: true},
-productname: {type: String, unique: true},
+productname: String,
 price: Float,  
 }
