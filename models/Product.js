73b74defa98acfedcb931d27ffe0e5acaf716010
Evan Cheney <70952698+epc33@users.noreply.github.com>
2020-09-24 08:49:56 -0400

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -4,4 +4,7 @@ const productSchema = new mongoose.Schema({
 productid: {type: String, unique: true},
 productname: String,
 price: Float,  
-}
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
